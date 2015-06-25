b11faabe1807a32df11b380dc8c6cc79d4ad459c
Dmytro Novikov <novikov.dmytro@gmail.com>
2015-06-25 10:01:16 +0300

    Implement dispatcher.

--- dispatcher.go
+++ dispatcher.go
@@ -2,9 +2,30 @@ package main
 
 import (
 	//"fmt"
+	"error"
 	zmq "github.com/pebbe/zmq4"
 )
 
+type Dispatcher struct {
+	zmqScoket *zmq.Socket
+	zmqPoller *zmq.Poller
+}
+
 func NewDispatcher(uri string) (*Dispatcher, error) {
-	zmqSocket, _ := zmq.NewSocket(zmq.ROUTER)
+
+	var dispatcher Dispatcher
+
+	if zmqSocket, err := zmq.NewSocket(zmq.ROUTER); err != nil {
+		return nil, err
+	}
+	if err = zmqSocket.Bind(uri); err != nil {
+		return nil, err
+	}
+	zmqPoller := zmq.NewPoller()
+	zmqPoller.Add(zmqSocket, zmq.POLLIN)
+
+	dispacther.zmqScoket = zmqScoket
+	dispacther.zmqPoller = zmqPoller
+
+	return &dispacther, err
 }
