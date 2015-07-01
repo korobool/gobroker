d6e66ce55681d4a0a6cbe9d733c09e6bbaffb938
Dmytro Novikov <novikov.dmytro@gmail.com>
2015-07-01 14:18:10 +0300

    Add cli option for url root path

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"html/template"
 	"io/ioutil"
@@ -8,12 +9,16 @@ import (
 	"net/http"
 )
 
-const rootPath = "http://localhost:8080"
+var rootPath string
 
 var GrossDispatcher Dispatcher
 var landingTempl *template.Template
 
 func main() {
+	root := flag.String("root", "localhost:8080", "server root")
+	flag.Parse()
+
+	rootPath = fmt.Sprintf("http://%s", *root)
 
 	// TODO: Do it properly (at least call a function load templates...)
 	landingTempl = template.New("valutchik.html")
